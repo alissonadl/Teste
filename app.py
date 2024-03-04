d89558e9ef096187a2bc18924ee72835dbed8a9b
Alisson Araújo de Lima <160548927+alissonadl@users.noreply.github.com>
2024-03-04 23:31:22 +0000

    aula 03

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ def alunos():
 @app.route('/professores')
 def professores():
     return render_template('professores.html')
+
+@app.route('/contato')
+def contato():
+    telefone = '1234-5678'
+    email = 'alisson@ifrn.com.br'
+    return render_template('contato.html', x=email, tel=telefone)
+
+@app.route('/usuario/<nome>')
+def usuario(nome):
+    return render_template('usuario.html', nome = nome)
