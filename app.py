1e9b281569af074fec6c4ce57b2e0f0a723fb466
Alisson Araújo de Lima <160548927+alissonadl@users.noreply.github.com>
2024-03-05 00:39:24 +0000

    aula 3

--- app.py
+++ app.py
@@ -20,6 +20,34 @@ def contato():
     email = 'alisson@ifrn.com.br'
     return render_template('contato.html', x=email, tel=telefone)
 
+'''
+#Colocando valor "anonimo" como valor padrão.
+
+@app.route('/usuario')
+@app.route('/usuario/')
+@app.route('/usuario/<nome>')
+def usuario(nome='anonimo'):
+    return render_template('usuario.html', nome = nome)
+'''
+
+#Forma alternativa e "correta" de definir valores padrão sem que o usuário passe.
+''
+@app.route('/usuario', defaults={"nome":"anônimo"})
+@app.route('/usuario/', defaults={"nome":"anônimo"})
 @app.route('/usuario/<nome>')
 def usuario(nome):
     return render_template('usuario.html', nome = nome)
+#'''
+
+''
+@app.route('/dobro/<int:n>')
+def dobro(n):
+    calculo = 2 * n
+    return 'O valor dobrado é: '+str(calculo)
+#'''
+
+#Colocando valores padrão inteiros
+@app.route('/dobrod/<int:n>')
+def dobrod(n):
+    calculod = 2 * n
+    return 'O valor dobrado é: ' + str(calculod)
